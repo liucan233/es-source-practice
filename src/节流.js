be7a7fef55bedc8234129aaf7a5cacb4364fc7d0
刘灿 <liucan.233@bytedance.com>
2022-09-06 21:51:36 +0800

    refactor: 节流函数支持this绑定

--- src/节流.js
+++ src/节流.js
@@ -1,11 +1,26 @@
-function throttle(task,delay){
-    let taskId=0;
-    return ()=>{
-        if(taskId===0){
-            taskId=setTimeout(()=>{
-                taskId=0;
-                task();
-            },delay);
-        }
+/**立即执行版 */
+function throttle(executor, delay) {
+  let awaiting = false;
+  return function (...args){
+    if (awaiting === false) {
+      awaiting = true;
+      setTimeout(() => {
+        awaiting = false;
+      }, delay);
+      executor.apply(this, args);
     }
+  };
 }
+
+const test = {
+  exec: throttle(function (msg){
+    console.log(this);
+    console.log("executed " + msg);
+  }, 1000),
+};
+
+test.exec(1);
+test.exec();
+
+setTimeout(test.exec, 900);
+setTimeout(test.exec.bind({tmp:2}), 1100, 2);
