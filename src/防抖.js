0b9fc30604d9272fe5f9abdcb82a02f3d8305509
刘灿 <liucan.233@bytedance.com>
2022-09-06 22:05:38 +0800

    refactor: 防抖函数支持this绑定

--- src/防抖.js
+++ src/防抖.js
@@ -1,42 +1,43 @@
 // 立即执行版
 function debounce1(task, delay) {
-    let taskId = 0;
-    return () => {
-        if (taskId ===0){
-            task();
-        } else {
-            clearTimeout(taskId);
-        }
-        taskId = setTimeout(() => {
-            taskId = 0;
-        },delay);
+  let awaitId = null;
+  return function () {
+    if (awaitId === null) {
+      task.apply(this, arguments);
+    } else {
+      clearTimeout(awaitId);
     }
+    awaitId = setTimeout(() => {
+      awaitId = null;
+    }, delay);
+  };
 }
 
 // 非立即执行版
-function debounce2(task,delay){
-    let taskId=0;
-    return ()=>{
-        if(taskId){
-            clearTimeout(taskId);
-            taskId=0;
-        }
-        taskId=setTimeout(task,delay);
+function debounce2(task, delay) {
+  let taskId = null;
+  return function () {
+    if (taskId) {
+      clearTimeout(taskId);
     }
+    taskId = setTimeout(task.bind(this, ...arguments), delay);
+  };
 }
 
-const c={
-    test(){
-        console.log(2333)
-    }
+const task=function(msg){
+    console.log(this)
+    console.log('executed '+msg)
 }
 
-const test=debounce(c.test,1000);
-test();
+const test = {
+  exec1: debounce1(task,1500),
+  exec2: debounce2(task,1500)
+};
 
-setTimeout(test,100);
-setTimeout(test,900);
-setTimeout(test,1800);
-setTimeout(test,2700);
-setTimeout(test,3600);
-test();
+test.exec2(1)
+test.exec2(2)
+test.exec2(3)
+
+setTimeout(test.exec2.bind({tmp:3},3), 100);
+setTimeout(test.exec2.bind({tmp:4},4), 900);
+setTimeout(test.exec2.bind({tmp:5},5), 1100);
